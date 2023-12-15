c886c92d7439264b48cb002aded29a9b5c0e2d54
andythebreaker <43373581+andythebreaker@users.noreply.github.com>
2023-12-15 11:12:27 +0800

    Update backburst4.py

--- backburst4.py
+++ backburst4.py
@@ -1,3 +1,4 @@
+import re
 from bs4 import BeautifulSoup
 
 def add_table_to_html(input_file_name):
@@ -12,36 +13,47 @@ def add_table_to_html(input_file_name):
     table = soup.find('table')
 
     if table:
-        # Create the new table structure
-        new_table = BeautifulSoup('''
-        <table data-toggle="table">
-            <thead>
-                <tr>
-                    <th data-field="name" data-sortable="true">name</th>
-                    <th data-field="create_date" data-sortable="true">create_date</th>
-                    <th data-field="update_date" data-sortable="true">update_date</th>
-                    <th data-field="JSU" data-sortable="true">JSU</th>
-                    <th data-field="grade" data-sortable="true">grade</th>
-                    <th data-field="subject" data-sortable="true">subject</th>
-                    <th data-field="url" data-sortable="true">url</th>
-                    <th data-field="download" data-sortable="false">download</th>
-                </tr>
-            </thead>
-            <tbody>
-            </tbody>
-        </table>
-        ''', 'html.parser')
 
-        # Replace the original table with the new table
-        table.replace_with(new_table)
+
+        #fix
+        # coding=utf8
+        # the above tag defines encoding for this document and is for Python 2.x compatibility
+
+
+        regex = r"<table>"
+
+        test_str = html_content
+        subst = "<table data-toggle=\"table\">            <thead>                <tr>                    <th data-field=\"name\" data-sortable=\"true\">name</th>                    <th data-field=\"create_date\" data-sortable=\"true\">create_date</th>                    <th data-field=\"update_date\" data-sortable=\"true\">update_date</th>                    <th data-field=\"JSU\" data-sortable=\"true\">JSU</th>                    <th data-field=\"grade\" data-sortable=\"true\">grade</th>                    <th data-field=\"subject\" data-sortable=\"true\">subject</th>                    <th data-field=\"url\" data-sortable=\"true\">url</th>                    <th data-field=\"download\" data-sortable=\"false\">download</th>                </tr>            </thead>            <tbody>"
+
+        # You can manually specify the number of replacements by changing the 4th argument
+        result = re.sub(regex, subst, test_str, 0, re.MULTILINE)
+
+        if result:
+            print (result)
+
+        # Note: for Python 2.7 compatibility, use ur"" to prefix the regex and u"" to prefix the test string and substitution.
+        # coding=utf8
+        # the above tag defines encoding for this document and is for Python 2.x compatibility
+
+        regex = r"</table>"
+
+        test_str = result
+        subst = "</tbody>        </table>"
+
+        # You can manually specify the number of replacements by changing the 4th argument
+        result = re.sub(regex, subst, test_str, 0, re.MULTILINE)
+
+        if result:
+            print (result)
+
+        # Note: for Python 2.7 compatibility, use ur"" to prefix the regex and u"" to prefix the test string and substitution.
 
         # Save the modified HTML to a new file or overwrite the original file
         with open(input_file_name, 'w') as output_file:
-            output_file.write(str(soup))
+            output_file.write(result)
         print("Table added successfully.")
+
     else:
         print("No table found in the HTML file.")
 
-# Usage example
-#input_file_name = 'your_input_file.html'
-#add_table_to_html(input_file_name)
+
