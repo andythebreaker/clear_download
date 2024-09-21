fa1ec662e077c53f78a85220fb86f587bd4df6bb
andythebreaker <43373581+andythebreaker@users.noreply.github.com>
2024-09-21 14:19:13 +0800

    add a var call viewDebug to 'not' print stuff to console

--- download_user_pic.py
+++ download_user_pic.py
@@ -10,49 +10,57 @@ import re
 from download_notebook_pic import ClearNotebooksScraper
 
 class ClearUsrScraper:
-    def __init__(self, usr_id):
+    def __init__(self, usr_id, viewDebug=False):
         self.usr_id = usr_id
-        self.ntList=[]
-        self.CONSTcn="https://www.clearnotebooks.com"
+        self.ntList = []
+        self.CONSTcn = "https://www.clearnotebooks.com"
+        self.viewDebug = viewDebug
 
-    def re_clusr(self,test_str):
-        print("=====fetch=====")
+    def re_clusr(self, test_str):
+        if self.viewDebug:
+            print("=====fetch=====")
         regex = r"\"path\":\"/zh-TW/notebooks/(\d+)\""
 
         matches = re.finditer(regex, test_str)
 
         for matchNum, match in enumerate(matches, start=1):
-            
-            print ("Match {matchNum} was found at {start}-{end}: {match}".format(matchNum = matchNum, start = match.start(), end = match.end(), match = match.group()))
+            if self.viewDebug:
+                print("Match {matchNum} was found at {start}-{end}: {match}".format(matchNum = matchNum, start = match.start(), end = match.end(), match = match.group()))
             
             for groupNum in range(0, len(match.groups())):
                 groupNum = groupNum + 1
-                print ("Group {groupNum} found at {start}-{end}: {group}".format(groupNum = groupNum, start = match.start(groupNum), end = match.end(groupNum), group = match.group(groupNum)))
+                if self.viewDebug:
+                    print("Group {groupNum} found at {start}-{end}: {group}".format(groupNum = groupNum, start = match.start(groupNum), end = match.end(groupNum), group = match.group(groupNum)))
                 self.ntList.append(match.group(groupNum))
         self.re_nx(test_str)
 
-    def re_nx(self,test_str):
-        print("=====next=====")
+    def re_nx(self, test_str):
+        if self.viewDebug:
+            print("=====next=====")
         #regex = r"<a class=\"boxed\" href=\"(/zh-TW/authors/\d+/explorer/notebooks\?author_id=\d+&amp;offset=\d+)\">\d+</a><span class='pagination-info'>"
         regex=r"<a class=\"boxed\" href=\"(/zh-TW/authors/\d+/explorer/notebooks\?author_id=\d+&amp;offset=\d+)\"><i class='fas fa-arrow-right'></i>"
         matches = re.search(regex, test_str)
 
         if matches:
-            print ("Match was found at {start}-{end}: {match}".format(start = matches.start(), end = matches.end(), match = matches.group()))
+            if self.viewDebug:
+                print("Match was found at {start}-{end}: {match}".format(start = matches.start(), end = matches.end(), match = matches.group()))
             
             for groupNum in range(0, len(matches.groups())):
                 groupNum = groupNum + 1
-                print ("Group {groupNum} found at {start}-{end}: {group}".format(groupNum = groupNum, start = matches.start(groupNum), end = matches.end(groupNum), group = matches.group(groupNum)))
+                if self.viewDebug:
+                    print("Group {groupNum} found at {start}-{end}: {group}".format(groupNum = groupNum, start = matches.start(groupNum), end = matches.end(groupNum), group = matches.group(groupNum)))
                 self.re_clusr(self.wget_me(f'{self.CONSTcn}{matches.group(groupNum)}'))
 
-    def wget_me(self,url):
-        print("=====wget=====")
+    def wget_me(self, url):
+        if self.viewDebug:
+            print("=====wget=====")
         url = url
         response = requests.get(url)
         return response.text
 
     def scrape_clear_usr(self):
-        print("=====get user notes (main) =====")
+        if self.viewDebug:
+            print("=====get user notes (main) =====")
         url = f'{self.CONSTcn}/zh-TW/authors/{self.usr_id}/explorer/notebooks'
         content=self.wget_me(url)
         self.re_clusr(content)
@@ -75,5 +83,5 @@ if __name__ == "__main__":
     if not usr_id:
         usr_id = input("Enter clear user id: ")
 
-    scraper = ClearUsrScraper(usr_id)
+    scraper = ClearUsrScraper(usr_id, viewDebug=False)
     scraper.scrape_clear_usr()
